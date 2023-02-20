3f4404b13bc8f475be9b9cfcfbc128481cecbb87
Sarada Talluri <124937457+TalluriSarada@users.noreply.github.com>
2023-02-20 11:15:13 -0600

    Add files via upload
    python labs from aws restart

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue1=3.14
+print(myValue1)
+print(type(myValue1))
+myValue2=5j
+print(myValue2)
+print(type(myValue2))
+myValue3=True
+print(myValue3)
+print(type(myValue3))
+myValue4=True
+print(myValue4)
+print(type(myValue4))
---
+++ stringconcatenation.py
@@ -0,0 +1,9 @@
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is your name? ")
+print("Hi",name,"Nice to see you")
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
